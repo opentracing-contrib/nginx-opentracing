4a51057c2e9ea0cc38ee4a4c31d49f9f03bb7b00
rnburn <contact@rnburn.com>
2017-04-25 13:33:03 -0400

    Corrected BUSY error from setup script.

--- example/tutorial/node/setup.js
+++ example/tutorial/node/setup.js
@@ -1,26 +1,27 @@
 const sqlite3 = require('sqlite3');
-const fs = require('fs');
 const common = require('./common');
 const program = require('commander');
+const winston = require('winston');
 const path = require('path');
 
 program.option('r, --data_root <data_root>', 'Data Root').parse(process.argv);
 
 if (typeof program.data_root === 'undefined') {
-  console.error('no daa_root given!');
+  winston.error('no data_root given!');
   process.exit(1);
-}
+  }
 
-databasePath = path.join(program.data_root, common.databaseName);
+const databasePath = path.join(program.data_root, common.databaseName);
+const db = new sqlite3.Database(databasePath);
 
-var db = new sqlite3.Database(databasePath);
-
-db.serialize(function() {
-  db.run(`create table if not exists animals (
+const stmt = `create table if not exists animals (
     uuid character(36) primary key,
     animal text not null,
     name text not null
-  )`);
+  )`;
+db.run(stmt, (err) => {
+  db.close();
+  if (err) {
+    winston.error('Failed to create animal table!', {statement : stmt});
+  }
 });
-
-db.close();
