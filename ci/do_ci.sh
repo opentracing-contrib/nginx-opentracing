c2d2e8141f12e92db1f00dcb73902f50b4e76707
Ryan Burn <ryan.burn@gmail.com>
2018-05-26 15:52:09 -0400

    Fix CI commands.

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -8,6 +8,6 @@ if [[ "$1" == "system.testing" ]]; then
   docker build -t nginx-opentracing-test/nginx -f Dockerfile-test .
   cd test2
   docker build -t nginx-opentracing-test/backend -f Dockerfile-backend .
-  python nginx_opentracing_test.py
+  python3 nginx_opentracing_test.py
   exit 0
 fi
