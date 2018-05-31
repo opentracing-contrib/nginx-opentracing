acc05f9342962c15f7a1b46f9b28391ceaa58f63
Ryan Burn <ryan.burn@gmail.com>
2018-05-31 17:35:36 -0400

    Remove hotrod and browser examples.

--- example/browser/node/servers.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-const childProc = require('child_process');
-
-childProc.fork(path.join(__dirname, 'frontend.js'));
-childProc.fork(path.join(__dirname, 'backend.js'));
