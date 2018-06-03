c55c12f3dd4016369aca58ccd6d77251a20f62bd
Ryan Burn <ryan.burn@gmail.com>
2018-06-03 19:03:41 -0400

    Push docker images upon release.

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -10,4 +10,16 @@ if [[ "$1" == "system.testing" ]]; then
   docker build -t nginx-opentracing-test/backend -f Dockerfile-backend .
   python3 nginx_opentracing_test.py
   exit 0
+elif [[ "$1" == "release" ]]; then
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+  VERSION_TAG="`git describe --abbrev=0 --tags`"
+  docker build -t opentracing/nginx-opentracing .
+  docker tag opentracing/nginx-opentracing opentracing/nginx-opentracing:${VERSION_TAG}
+  docker push opentracing/nginx-opentracing:${VERSION_TAG}
+  docker tag opentracing/nginx-opentracing opentracing/nginx-opentracing:latest
+  docker push opentracing/nginx-opentracing:latest
+  exit 0
+else
+  echo "Invalid do_ci.sh target"
+  exit 1
 fi
