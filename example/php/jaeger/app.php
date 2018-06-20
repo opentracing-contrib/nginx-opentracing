80f9cc09af304848090c4ebae55639ec96d1ad24
Ryan Burn <ryan.burn@gmail.com>
2018-06-19 22:02:57 -0400

    Remove dead code.

--- example/php/jaeger/app.php
+++ example/php/jaeger/app.php
@@ -11,7 +11,6 @@ if (!function_exists('getallheaders')) {
     $headers = [];
     foreach ($_SERVER as $name => $value) {
         if (substr($name, 0, 5) == 'HTTP_') {
-            /* $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; */
             $headers[str_replace(' ', '-', strtolower(str_replace('_', ' ', substr($name, 5))))] = $value;
         }
     }
