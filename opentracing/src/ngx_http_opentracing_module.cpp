5293449411b1f2009fe61fc358d3da6409f5ae5b
Ryan <ryan.burn@gmail.com>
2018-09-16 11:46:22 -0700

    Support opentracing_fastcgi_propagate_context main and server blocks. (#59)

--- opentracing/src/ngx_http_opentracing_module.cpp
+++ opentracing/src/ngx_http_opentracing_module.cpp
@@ -219,8 +219,10 @@ static ngx_command_t opentracing_commands[] = {
          NGX_CONF_NOARGS,
      propagate_opentracing_context, NGX_HTTP_LOC_CONF_OFFSET, 0, nullptr},
     {ngx_string("opentracing_fastcgi_propagate_context"),
-     NGX_HTTP_LOC_CONF | NGX_CONF_NOARGS, propagate_fastcgi_opentracing_context,
-     NGX_HTTP_LOC_CONF_OFFSET, 0, nullptr},
+     NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF |
+         NGX_CONF_NOARGS,
+     propagate_fastcgi_opentracing_context, NGX_HTTP_LOC_CONF_OFFSET, 0,
+     nullptr},
     {ngx_string("opentracing_operation_name"),
      NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF |
          NGX_CONF_TAKE1,
