2e9ced376f0330b7ade9ac7a9df1fc462d5b0ef9
Ryan Burn <ryan.burn@gmail.com>
2019-01-17 16:36:34 -0800

    Upgrade to OpenTracing-1.5.1.

--- ci/install_opentracing.sh
+++ ci/install_opentracing.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-[ -z "${OPENTRACING_VERSION}" ] && export OPENTRACING_VERSION="v1.5.0"
+[ -z "${OPENTRACING_VERSION}" ] && export OPENTRACING_VERSION="v1.5.1"
 
 # Build OpenTracing
 cd /
