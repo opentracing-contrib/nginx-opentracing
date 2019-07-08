c423756205d4c3b3db77b319315ba2a75d90f389
Geoff Speirs <geoff.speirs@myob.com>
2019-07-09 06:51:21 +1000

    test

--- ci/build_module_binaries.sh
+++ ci/build_module_binaries.sh
@@ -3,7 +3,7 @@
 set -e
 
 export OPENTRACING_VERSION=1.5.1
-NGINX_VERSIONS=(1.16.0 1.15.8 1.15.1 1.15.0 1.14.0 1.13.12 1.13.6 1.12.2)
+NGINX_VERSIONS=(1.15.1 1.15.0 1.14.0 1.13.12 1.13.6 1.12.2)
 
 # Compile for a portable cpu architecture
 export CFLAGS="-march=x86-64 -fPIC"
