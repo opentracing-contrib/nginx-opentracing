b01d555665951a85a91f1b5e9e1fead4d20f6326
Michael Nikitochkin <michael.nikitochkin@shopify.com>
2020-12-07 12:01:40 +0100

    Upgrade Docker image to support Opentracing 1.6.0
    Among all tracers only Jaeger fully supports Opentracing 1.6.0.
    Remove all tracers, because are not fully supported by communities.
    Jaeger covers a lot of formats: Zippkin and W3C.

--- ci/install_opentracing.sh
+++ ci/install_opentracing.sh
@@ -3,6 +3,7 @@
 set -e
 
 [ -z "${OPENTRACING_VERSION}" ] && export OPENTRACING_VERSION="v1.5.1"
+# [ -z "${OPENTRACING_VERSION}" ] && export OPENTRACING_VERSION="v1.6.0"
 
 # Build OpenTracing
 cd /
