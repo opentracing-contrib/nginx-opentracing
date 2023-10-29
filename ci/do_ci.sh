c59aa6d9953677a1976ac9be8714064604a59bea
Luca Comellini <luca.com@gmail.com>
2023-10-28 18:59:43 -0700

    Move binaries build to GitHub Actions

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -4,7 +4,6 @@ set -e
 
 [ -z "${SRC_DIR}" ] && export SRC_DIR="$(pwd)"
 [ -z "${BUILD_DIR}" ] && export BUILD_DIR=/build
-[ -z "${MODULE_DIR}" ] && export MODULE_DIR=/modules
 
 if [[ "$1" == "system.testing" ]]; then
   docker build -t nginx-opentracing-test/nginx -f Dockerfile-test .
@@ -19,11 +18,6 @@ elif [[ "$1" == "build" ]]; then
   mkdir -p "${BUILD_DIR}"
   ./ci/build_nginx_opentracing_module.sh
   exit 0
-elif [[ "$1" == "module.binaries" ]]; then
-  mkdir -p "${BUILD_DIR}"
-  mkdir -p "${MODULE_DIR}"
-  ./ci/build_module_binaries.sh
-  exit 0
 elif [[ "$1" == "push_docker_image" ]]; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   VERSION_TAG="$(git describe --abbrev=0 --tags)"
@@ -35,9 +29,6 @@ elif [[ "$1" == "push_docker_image" ]]; then
   docker push opentracing/openresty:${VERSION}
   docker tag opentracing/openresty opentracing/openresty:latest
   docker push opentracing/openresty:latest
-  exit 0
-elif [[ "$1" == "release" ]]; then
-  ./ci/release.sh
   exit 0
 else
   echo "Invalid do_ci.sh target"
